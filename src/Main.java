58c231899aff42acbcaa730bee04be551caea40f
jemi-dev <jemimaaka02@gmail.com>
2026-05-09 16:26:53 +0000

    Génération d'un nombre entier entre 1 et 100

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        /* generation d'un nombre entier compris entre 1 et 100 */
+        int nombre= (int)(Math.random() * 100) +1;
+        System.out.println("Bienvenue à Deviner un nombre ");
+        System.out.println("Vous devez deviner un nombre compris entre 1 et 100 inclus");
+
     }
 }
